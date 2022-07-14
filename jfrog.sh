d83001be29b31edac5d8dd39359a195aaa67eced
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-14 13:34:33 +0530

    Update jfrog.sh

--- jfrog.sh
+++ jfrog.sh
@@ -4,4 +4,4 @@ sudo apt install wget -y
 wget -qO - https://api.bintray.com/orgs/jfrog/keys/gpg/public.key | sudo apt-key add -
 echo "deb https://jfrog.bintray.com/artifactory-debs bionic main" | sudo tee /etc/apt/sources.list.d/jfrog.list
 sudo apt update
-sudo apt install jfrog-artifactory-oss
+sudo apt install jfrog-artifactory-oss -y
